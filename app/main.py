7b9d7f038fd5305b5d5b06da7df37233bfa9f8db
Aharon Katz <ahkatz@g.jct.ac.il>
2025-05-28 13:33:23 +0300

    Enable CORS for frontend

--- app/main.py
+++ app/main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
 from .crush import crush
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # אפשר לשים ['http://localhost:63343'] אם את רוצה להגביל
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class BoardInput(BaseModel):
     board: List[List[int]]
